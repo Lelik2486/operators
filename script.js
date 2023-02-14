1809525a2e54f3239202aa1e055e94b8b7a0bcfd
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2023-02-14 14:29:14 +0300

    add info

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 "use strict"
 
+// Конструкция if else
 if (4 == 9) {
   console.log('OK');
+} else {
+  console.log('Error');
 }
+
+const num = 50;
+
+if (num < 49) {
+  console.log('Error');
+} else if (num > 100) {
+  console.log('Много');
+} else {
+  console.log('OK!');
+}
+
+// конструкция тернарного оператора
+//                   true               false
+(num === 50) ? console.log('OK!') : console.log('Error'); 
+
